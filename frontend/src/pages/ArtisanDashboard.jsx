16250fe33fda68fa918e7d66cca1940c34bcfb34
cosuoji <qelvinz@gmail.com>
2026-03-07 18:36:51 +0100

    Update ArtisanDashboard.jsx

--- frontend/src/pages/ArtisanDashboard.jsx
+++ frontend/src/pages/ArtisanDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import API from "../api/axios";
 import toast from "react-hot-toast";
 import ArtisanLocationPicker from "../components/ArtisanLocationPicker";
